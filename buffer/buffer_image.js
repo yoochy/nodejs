99ed64f860f87cd668c817b5b3163aab207dc231
yoochy <yhywz@qq.com>
2017-05-03 15:06:01 +0800

    新2 Signed-off-by: yoochy <yhywz@qq.com>

--- buffer/buffer_image.js
+++ buffer/buffer_image.js
@@ -8,7 +8,7 @@ fs.readFile('logo.png',function(err,origin_buffer){
 	console.log(base64Image)
 	var decodedImage = new Buffer(base64Image,'base64')
 	console.log(Buffer.compare(origin_buffer,decodedImage))
-	fs.writeFile('logo_buffer.png',decodedImage,function(err){
+	fs.writeFile('logo_decoded.png',decodedImage,function(err){
 		if(err) console.log(err)
 	})
 })
